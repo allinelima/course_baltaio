d609313c55dff2bdfb56c0368f39a69c6c26426f
Aline Lima <aline.lima@neon.com.br>
2023-06-05 15:19:21 -0300

    feat: add list career

--- ContentContext/Content.cs
+++ ContentContext/Content.cs
@@ -7,14 +7,16 @@ namespace Balta.ContentContext
 {
     public abstract class Content
     {
-        public Content()
+        public Content(string title, string url)
         {
             Id = Guid.NewGuid();
+            Title = title;
+            Url = url;
         }
 
         public Guid Id { get; set; }
-        public int Title { get; set; }
-        public int Url { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
         
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,21 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Balta.ContentContext;
+using course_baltaio.ContentContext;
 
 namespace course_baltaio
 {
-    public class Program
+     class Program
     {
          static void Main (string[] args)
         {
-            Console.WriteLine("Test");
-            var course = new Course();
-            course.Level = ContentContext.Enums.EContentLevel.Beginner;
-            foreach(var item in course.Modules)
+            var articles = new List<Article>();
+            articles.Add(new Article("Artigo sobre OOP", "orientacao-objetos.io"));
+            articles.Add(new Article("Artigo sobre C#", "csharp.io"));
+            articles.Add(new Article("Artigo sobre .NET", "dotnet.io"));
+
+            foreach(var article in articles)
             {
-                
+                Console.WriteLine(article.Id);
+                Console.WriteLine(article.Title);
+                Console.WriteLine(article.Url);
             }
 
+            var courses = new List<Course>();
+            var courseOOP = new Course("Fundamentos OOP", "fundamentos-oop.io");
+            var courseCsharp = new Course("C#", "fundamentos-csharp.io");
+            var courseAspNet = new Course("ASP .Net", "fundamentos-aspdotnet.io");
+            courses.Add(courseOOP);
+            courses.Add(courseCsharp);
+            courses.Add(courseAspNet);
+
+            var careers = new List<Career>();
+            var careerDotnet = new Career("Especialista .NET" , "especialista-dotnet.io");
+            var careerItem = new CareerItem( 1, "Comece por aqui", "", null);
+            careerDotnet.Items.Add(careerItem);
+            careers.Add(careerDotnet);
+
+            foreach(var career in careers){
+                Console.WriteLine(career.Title);
+                foreach (var item in career.Items)
+                {
+                    Console.WriteLine($"{item.Order} - {item.Title}");
+                }
+            }
         }
     }
 }
